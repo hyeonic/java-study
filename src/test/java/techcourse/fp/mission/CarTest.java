0173b8c4de5c1254fb7147c70e86107bcf3cdaba
hyeonic <dev.hyeonic@gmail.com>
2022-03-25 11:27:24 +0900

    feat: 미션 1 CarTest MoveStrategy 람다를 활용하여 생성

--- src/test/java/techcourse/fp/mission/CarTest.java
+++ src/test/java/techcourse/fp/mission/CarTest.java
@@ -30,4 +30,22 @@ class CarTest {
         });
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
+
+    @Test
+    public void 이동_람다() {
+        Car car = new Car("pobi", 0);
+
+        Car actual = car.move(() ->  true);
+
+        assertThat(actual).isEqualTo(new Car("pobi", 1));
+    }
+
+    @Test
+    public void 정지_람다() {
+        Car car = new Car("pobi", 0);
+
+        Car actual = car.move(() -> false);
+
+        assertThat(actual).isEqualTo(new Car("pobi", 0));
+    }
 }
