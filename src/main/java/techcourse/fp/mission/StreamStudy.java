2ecade45668737bbf8d4b51245de6c371878db5e
hyeonic <dev.hyeonic@gmail.com>
2022-03-25 12:03:45 +0900

    feat: map, reduce, filter 실습 추가

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -1,44 +1,66 @@
 package techcourse.fp.mission;
 
+import static java.util.stream.Collectors.toList;
+
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StreamStudy {
 
+//    public static long countWords() throws IOException {
+//        String contents = Files.readString(Paths
+//            .get("src/main/resources/fp/war-and-peace.txt"));
+//        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+//
+//        long count = 0;
+//        for (String w : words) {
+//            if (w.length() > 12) count++;
+//        }
+//        return count;
+//    }
+
     public static long countWords() throws IOException {
         String contents = Files.readString(Paths
-            .get("src/main/resources/fp/war-and-peace.txt"));
+                .get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(word -> word.length() > 12)
+                .count();
     }
+
+//    public static List<Integer> doubleNumbers(List<Integer> numbers) {
+//        List<Integer> result = new ArrayList<>();
+//        for (Integer number : numbers) {
+//            result.add(2 * number);
+//        }
+//
+//        return result;
+//    }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer number : numbers) {
-            result.add(2 * number);
-        }
-
-        return result;
+        return numbers.stream()
+                .map(number -> number * 2)
+                .collect(toList());
     }
 
+//    public static long sumAll(List<Integer> numbers) {
+//        int result = 0;
+//
+//        for (Integer number : numbers) {
+//            result += number;
+//        }
+//
+//        return result;
+//    }
+
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for (Integer number : numbers) {
-            result += number;
-        }
-
-        return result;
+        return numbers.stream()
+                .reduce((a, b) -> a + b)
+                .get();
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
